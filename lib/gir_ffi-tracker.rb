c9abfb7b22bd1706e7bd69626faf2aadcb8b4de7
Matijs van Zuijlen <matijs@matijs.net>
2012-04-09 12:15:51 +0200

    Avoid #get_string destroying its own override.

--- lib/gir_ffi-tracker.rb
+++ lib/gir_ffi-tracker.rb
@@ -3,7 +3,11 @@ require 'gir_ffi'
 GirFFI.setup :Tracker
 
 module Tracker
-  SparqlCursor.class_eval do
+  load_class :SparqlCursor
+
+  class SparqlCursor
+    _setup_instance_method "get_string"
+
     def get_string_with_hidden_length column
       result, _ = *get_string_without_hidden_length(column)
       result
--- test/unit/tracker_sparql_cursor_test.rb
+++ test/unit/tracker_sparql_cursor_test.rb
@@ -2,12 +2,20 @@ require File.expand_path('../test_helper.rb', File.dirname(__FILE__))
 
 describe Tracker::SparqlCursor do
   describe "#get_string" do
-    it "returns just the string value" do
+    before do
       conn = Tracker::SparqlConnection.get_direct nil
-      cursor = conn.query "SELECT ?url WHERE { ?x a nie:InformationElement; nie:url ?url. }", nil
+      @cursor = conn.query "SELECT ?url WHERE { ?x a nie:InformationElement; nie:url ?url. }", nil
 
-      cursor.next nil
-      assert_instance_of String, cursor.get_string(0)
+      @cursor.next nil
+    end
+
+    it "returns just the string value" do
+      assert_instance_of String, @cursor.get_string(0)
+    end
+
+    it "can safely be called twice" do
+      @cursor.get_string(0)
+      assert_instance_of String, @cursor.get_string(0)
     end
   end
 end
