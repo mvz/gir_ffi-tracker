75282cc6a5e870f1ec6f0fd9e9f160e6e298aa21
Matijs van Zuijlen <matijs@matijs.net>
2012-04-09 18:38:20 +0200

    Use simpler SPARQL query that does not require Tracker to have data.

--- test/unit/tracker_sparql_cursor_test.rb
+++ test/unit/tracker_sparql_cursor_test.rb
@@ -4,18 +4,18 @@ describe Tracker::SparqlCursor do
   describe "#get_string" do
     before do
       conn = Tracker::SparqlConnection.get_direct nil
-      @cursor = conn.query "SELECT ?url WHERE { ?x a nie:InformationElement; nie:url ?url. }", nil
+      @cursor = conn.query "SELECT 'Foo' { }", nil
 
       @cursor.next nil
     end
 
     it "returns just the string value" do
-      assert_instance_of String, @cursor.get_string(0)
+      @cursor.get_string(0).must_equal "Foo"
     end
 
     it "can safely be called twice" do
       @cursor.get_string(0)
-      assert_instance_of String, @cursor.get_string(0)
+      @cursor.get_string(0).must_equal "Foo"
     end
   end
 end
